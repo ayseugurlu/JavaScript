beb120a358768f7a18554b59450953f649ba8329
ayseugurlu <ayseugurlu9135@gmail.com>
2024-05-14 16:14:40 +0200

    fertig

--- Greatest-Comon-Divisor/app.js
+++ Greatest-Comon-Divisor/app.js
@@ -1,4 +1,20 @@
-let a ,b ;
-
-while(i<=a && i <=b){
-    if(a%i==0 && b%i==0)
+let a = +prompt("bir sayi giriniz");
+let b = +prompt("bir sayi giriniz");
+a = a < 0 ? -a : a;
+b = b < 0 ? -b : b;
+let i = 1;
+let gcd = 1;
+if (a <= b) {
+  for (i = 1; i <= a; i++) {
+    if (a % i == 0 && b % i == 0) {
+      gcd = i;
+    }
+  }
+} else {
+  for (i = 1; i <= b; i++) {
+    if (a % i == 0 && b % i == 0) {
+      gcd = i;
+    }
+  }
+}
+console.log(gcd);
