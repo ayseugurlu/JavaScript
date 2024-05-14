c9b6c82a9fc6803adf8867b78fabdb4655f82e27
ayseugurlu <ayseugurlu9135@gmail.com>
2024-05-14 14:41:40 +0200

    neu

---
+++ Greatest-Comon-Divisor/app.js
@@ -0,0 +1,4 @@
+let a ,b ;
+
+while(i<=a && i <=b){
+    if(a%i==0 && b%i==0)
---
+++ javascript/Classnotes/JS-03-Conditionals/short-curcuit/for.js
@@ -0,0 +1,39 @@
+// let i=1;
+// let sum=0;
+// for(i=1; i<=100; i++){
+//     sum=sum+i;
+   
+// }
+// console.log(sum);
+
+
+// let sqr;
+// for(let n=1 ; n<=10 ; n++){
+//     sqr=n**2;
+//     console.log(sqr);
+// }
+
+
+// let fak=1;
+// let a=+prompt("bir sayi giriniz");
+// for(let i=1 ; i<=a; i--){
+//     fak=fak*i;
+
+// }
+
+// console.log(fak);
+
+// let sum=0;
+// let sqr;
+// let i;
+// for( i=1 ; i<=4 ; i++){
+//     for(let j=1 ; j<=i ; j++){
+//          sqr=j**2;
+//          console.log(sqr);
+//     }
+//     sum=sum+sqr;
+// }
+
+// console.log(sum);
+
+
