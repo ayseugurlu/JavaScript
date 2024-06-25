2dcc3622eca6b30dfc43e0190e1f72e8f58f4697
ayseugurlu <ayseugurlu9135@gmail.com>
2024-06-25 10:47:57 +0200

    project1 beended

---
+++ javascript/Classnotes/JS-27-Async-Project1/meal.js
@@ -0,0 +1,48 @@
+//www.themealdb.com/
+
+let dizi=[]
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+  .then((resp) => resp.json())
+  .then((veri) =>{ 
+    dizi=veri
+    showScreen(veri.meals)
+})
+    
+   
+
+
+
+  //!EKRANA BASTIRMA
+
+  function showScreen(data){
+const mealsDiv= document.querySelector(".food")
+
+mealsDiv.innerHTML=""
+data.forEach((a)=>{
+
+mealsDiv.innerHTML += `
+<div class="col-md-3 m-1">
+<p>${a.strMeal} </p>
+<img width="200px" src="${a.strMealThumb}"/>
+</div>
+
+`;
+
+
+})
+
+  }
+
+//!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
+
+//? e.target.value=> inputa her veri girişindeki değerleri alabilmek için kullanılır, mesela onclick te sadece e.value kullanılır, çünkü inputla işimiz bitince onclick ile inputtaki veriyi topla git
+
+document.querySelector("input").oninput = (e) => {
+    // console.log(e.target.value)
+
+   let veri = dizi.meals.filter((a) => a.strMeal.toLowerCase().includes(e.target.value.toLowerCase()))
+
+   showScreen(veri)
+}
+
